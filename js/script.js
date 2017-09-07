8d64ba9441e6d4ea4155f27c07b2cd08b1cf126c
Yuriy Shandor <yuriyshandor@gmail.com>
2017-09-07 13:53:57 +0300

    Add Section Cinema-Movies (Mod_02)

--- js/script.js
+++ js/script.js
@@ -11,4 +11,16 @@ $(document).ready(function() {
     prevArrow: $('.top-slider__nav_previous'),
     nextArrow: $('.top-slider__nav_next'),
   });
+
+  $('.cinema-movie__nav_item:first-child').addClass('cinema-movie__nav_item_on-click');
+  $('.cinema-movie__links_movies:first-child').addClass('cinema-movie__links_movies_on-click');
+  $('.cinema-movie__nav_item').click(function() {
+    $('.cinema-movie__nav_item').removeClass('cinema-movie__nav_item_on-click');
+    $(this).addClass('cinema-movie__nav_item_on-click');
+    var indexOf = $(this).index()+1;
+    $('.cinema-movie__links_movies').removeClass('cinema-movie__links_movies_on-click');
+    $(`.cinema-movie__links_movies:nth-child(${indexOf})`).addClass('cinema-movie__links_movies_on-click');
+  })
+
+  
 });
